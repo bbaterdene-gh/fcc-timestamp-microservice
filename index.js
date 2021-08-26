bd1121278d3b435ddff6520decfd177fdac0cc12
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-26 22:51:17 +0800

    timestamp complete

--- index.js
+++ index.js
@@ -3,8 +3,44 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Wo2rld!')
+app.get('/api/:date?', (req, res) => {
+  let { date } = req.params
+  // no date
+  if (!date) {
+    date = new Date()
+    res.send({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    })
+  }
+
+  date = date.split('-')
+
+  // epoch
+  if ( date.length === 1) {
+    date = new Date(+date[0])
+  } else if ( date.length <= 3 ){
+  // year, month, day
+    const year = +date[0] || 0
+    const month = date[1] ? +date[1]-1 : 0
+    const day = date[2] ? +date[2] : 1
+
+    date = new Date()
+    date.setUTCFullYear(year)
+    date.setUTCMonth(month)
+    date.setUTCDate(day)
+    date.setUTCHours(0, 0, 0, 0)
+  } else {
+    res.status(400)
+    res.send({
+      error: "Invalid Date"
+    })
+  }
+
+  res.send({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  })
 })
 
 app.listen(port, () => {
